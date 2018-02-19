7eaa61b456769242e71a4f82adc3a010bd7e4eca
Harvey Bal <harvey_bal@hotmail.co.uk>
2018-02-19 20:02:02 +0000

    Added ouput if maze is unsolvable

--- src/main/java/com/hrv/mazeSolver/maze/Maze.java
+++ src/main/java/com/hrv/mazeSolver/maze/Maze.java
@@ -8,6 +8,7 @@ public class Maze {
     private List<MazeSection> maze = new ArrayList<MazeSection>();
     private int width;
     private int height;
+    private boolean solved = false;
 
     public Maze(int width, int height){
         this.width=width;
@@ -32,6 +33,11 @@ public class Maze {
     }
 
     public void print(){
+        if(solved){
+            System.out.println("Solved");
+        } else {
+            System.out.println("Unsolved");
+        }
         for(int i=0; i<maze.size(); i++){
             if(i%width<width-1){
                 System.out.print(maze.get(i));
